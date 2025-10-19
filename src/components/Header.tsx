c493e7a386353274d0e1f1f89b584084ff824407
tsunyoku <tsunyoku@gmail.com>
2025-10-20 00:40:58 +0100

    change query param

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -159,7 +159,7 @@ export default function Header() {
               {context.user != null ? (
                 <ProfileMenu context={context} setContext={setContext} />
               ) : (
-                <Link to={`https://blog-api.tsunyoku.xyz/auth/login?redirectUri=${window.location.href}`}>
+                <Link to={`https://blog-api.tsunyoku.xyz/auth/login?redirect_uri=${window.location.href}`}>
                   <Button variant="contained">
                     <Typography variant="subtitle1">Login</Typography>
                   </Button>
